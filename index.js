32bac101260c6820950308352e74fd98f9ea4e39
Sherry Lin <sherrylin222@gmail.com>
2018-05-20 03:32:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
   for (var i = 0; i < musicians.length; i++) {
-    return musicians[i] + " plays " + instruments[i]
+    return newArray.push = musicians[i] + " plays " + instruments[i];
   }
 
 }
